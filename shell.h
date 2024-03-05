ceaf64ebfd3ef92ac764b0c55d55a7ee67cb0a06
ZachYu-1 <zhekunyu9@gmail.com>
2024-03-05 10:48:07 -0500

    new file:   main.c 	new file:   read_line.c 	new file:   shell.h 	new file:   shell_interactive.c

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/*---LIBRARIES---*/
+#include <stdio.h>
+#include <unistd.h>
+
+/*---PROTOTYPES---*/
+/* main.c */
+void shell_interactive(void);
+void shell_no_interactive(void);
+
+#endif
